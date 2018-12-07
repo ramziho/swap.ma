e7b455672ec5eca619ec02db36d98efa21ce625b
AyoubMarouan <ayoub.marouan.97@gmail.com>
2018-12-07 16:18:01 +0000

    Update user.php

--- model/user.php
+++ model/user.php
@@ -2,21 +2,21 @@
 
 	class user{
 
-		function loadUser( $userId  ){
+		static function loadUser( $userId  ){
 			return dbGets(" select * from users where id= $userId ");
 		}
 
-		function addUser( $userId , $email , $lastName, $firstName , $password ,  $pathPhoto , $tel   , $type ){
+		static function addUser( $userId , $email , $lastName, $firstName , $password ,  $pathPhoto , $tel   , $type ){
 			$added=dbPost("insert into users values ( $userId,'$firstName','$lastName','$email','$password','$tel','$pathPhoto',now() , ‘$type' )"   );
 			return $added;
 		}
 		
-		function editUser( $userId  , $email , $lastName, $firstName , $password ,  $pathPhoto , $tel   , $type  ){
+		static function editUser( $userId  , $email , $lastName, $firstName , $password ,  $pathPhoto , $tel   , $type  ){
 			$modified=dbPost("update users set first_name='$firstName', last_name='$lastName', email='$email', password='$password', tel='$tel', image_path='$pathPhoto' ,type= ‘$type'  where id=$userId " );
 				return $modified;
 		}
 
-		function editChamp($id , $champ , $value){
+		static function editChamp($id , $champ , $value){
 			$modified=dbPost("update users set $champ='$value' where id=$id ");
 			return $modified;
 		}
