f8767d9c2ef569def32c4088ffe741c11d3a2f3e
AyoubMarouan <ayoub.marouan.97@gmail.com>
2018-12-07 17:17:53 +0000

    Update product.php

--- model/product.php
+++ model/product.php
@@ -1,6 +1,10 @@
 <?
 	/*
 		loadProduct
+		loadProductsUser
+		loadProductsByCategory
+		loadProductsBySearch
+		loadProductsByNearMe
 		addProduct
 		addImages
 		removeImage
@@ -10,12 +14,38 @@
 		editColumn
 	*/
 	
+    <?
+
     class product{
         
         static function loadProduct( $id ){
-            return $dbGet("SELECT * FROM product WHERE id = $id");
+            return dbGet("SELECT * FROM product WHERE id = $id");
         }
-            
+        
+        static function loadProductsUser($idUser){ 
+            return dbGets( "SELECT * FROM product WHERE id_user = $idUser" );
+        }
+        
+        static function loadProductsByCategory( $cat ){
+            return dbGets("SELECT * FROM product WHERE id_category = $cat");
+        }
+        
+        static function loadProductsBySearch($search ,$id_category=0){
+       $cat="";
+       if($id_category!=0)
+       $cat=" and id_category=$id_category ";
+        return dbGets("select * from product where title like '%$search%' or description like '%$search%' $cat order by post_date ");
+        }
+        
+        static function loadProductsByNearMe( $long , $lat ){
+        $query="
+            set @lat=$lat;
+            set @lng=$long;
+            SELECT *, 111.045 * DEGREES(ACOS(COS(RADIANS(@lat)) * COS(RADIANS(latitude)) *         COS(RADIANS(longtitude) - RADIANS(@lng)) + SIN(RADIANS(@lat)) * SIN(RADIANS(latitude)))) AS distance_in_km FROM product ORDER BY distance_in_km 
+            ";
+            return dbGets( $query );
+        }
+        
         static function addProduct($title,$description,$id_category,$value,$id_user,$id_ville,$long,$la,$list_photo){
             $added=dbPost("INSERT INTO `product` (`id`, `title`, `description`, `id_category`, `value`, `id_user`, `id_ville`, `longtitude`, `latitude`, `status`, `post_date`) VALUES (NULL, '$title', '$description', '$id_category', '$value', '$id_user', '$id_ville', '$long', '$la', 'pending', now())");     
            return dbGetId();
@@ -48,5 +78,15 @@
         }
         
     }
+
+
+
+
+
+
+
+
+
+?>
 
 ?>
