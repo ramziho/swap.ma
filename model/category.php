e046ec97186e295941c56472f621d0cc3f8fd025
AyoubMarouan <ayoub.marouan.97@gmail.com>
2018-12-08 15:14:08 +0000

    sdqs
    qsdqsd

---
+++ model/category.php
@@ -0,0 +1,17 @@
+<?php
+  
+    class  category{
+        
+  			static function getMainCategory(){
+          	$data = dbGets("SELECT * from main_category");
+          return $data;
+        }
+    		
+  			static function getCatogories( $idMainCategory ){
+           $data = dbGets("SELECT * FROM category WHERE id_main_category = $idMainCategory  "); 
+           return $data;
+        }
+  	
+    }
+
+?>
